8eac7019d9e245bd2182d943770b344133b2c9e5
andreadiblasi <andrea.diblasix@gmail.com>
2018-02-28 16:46:39 +0100

    [A.D] update

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,56 @@ new Vue({
     el: '#app',
     data: {
         monsterHealth: 100,
-        playerHealth: 100
+        playerHealth: 100,
+        actions: []
     },
-    computed: {
-        attack: function(){
-            return this.monsterHealth - 5;
+    computed:{
+        result: function(){
+            return this.monsterHealth <= 3 ? 'Hai vinto' : '';
+        }
+    },
+    methods: {
+        newGame: function(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.actions = [];
+        },
+        heal: function(){
+            if(this.playerHealth >= 100){
+                return;
+            }
+            this.actions.push({
+                type: "health",
+                value: 20   
+            });
+            this.playerHealth += 20;
+            this.monsterAttack();   
+        },
+        attack: function (){
+            this.actions.push({
+                type: "monster",
+                value: 5    
+            });
+            this.monsterAttack();
+
+            return this.monsterHealth -= 5;
         },
         specialAttack: function(){
-            return this.monsterHealth - 15;
+            this.actions.push({
+                type: "monster",
+                value: 15    
+            });
+            this.monsterAttack();
+            return this.monsterHealth -= 15;
+        },
+        monsterAttack: function() {
+            var randomValue = Math.floor((Math.random() * 15) + 1);
+            this.actions.push({
+                type: "user",
+                value: randomValue  
+            });
+            return this.playerHealth -= randomValue;
         }
+
     }
 })
